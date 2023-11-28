e8c16965e32c221fdb77629b1ecc9ac179876972
salabdulk <143503758+salabdulk@users.noreply.github.com>
2023-11-28 10:51:38 +0100

    Structure des dossiers

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * ETML
+ * Auteur:
+ * Date:
+ * Description: Information sur la connexion à la base de données. Administrateur: Login--> Edouard, Mot de passe--> Admin. Utilisateur: Login--> Albert, Mot de passse--> User
+ */
+
+return[
+    'host' => 'localhost',
+    'port' => '6033',
+    'dbname' => 'db_library',
+    'username' => 'root',
+    'password' => 'root'
+];
