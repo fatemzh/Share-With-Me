74dda171125da105d85df9b7ffd9181e7fdff3cb
cyberkathleen <67641959+cyberkathleen@users.noreply.github.com>
2023-12-05 09:24:34 +0100

    Move Database.php

---
+++ src/catalog.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * 
+ * ETML
+ * Auteur : Kathleen Lu
+ * Date : 05.12.23
+ * Description : Page de catalogue du site avec la liste de tous les livres filtrable par catégorie
+ */
+
+include('Database.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./css/styles.css" rel="stylesheet">
+    <title>Bibliothèque | Catalogue</title>
+</head>
+<body>
+    
+</body>
+</html>
