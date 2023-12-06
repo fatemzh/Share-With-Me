98886429ccfa25c88b82a9d18a9a92305e9d76f9
Salma AbdulkadirMohamed <salabdulkad@etmlnet.local>
2023-12-06 11:22:44 +0100

    Fin leçon 6.12.2023 --> css presque fini

--- src/details.php
+++ src/details.php
@@ -14,65 +14,68 @@
             <?php include("./parts/nav.inc.php") ?>
         </header>
         <main>
-        <div id="breadcrumb">
-                <div class="link"><div class="CATALOG"><a href="./catalog.php">CATALOGUE</a> /</div></div>
-                <div class="link"><div class="CATALOG"><a href="">ROMANCE</a> /</div></div>
-                <div class="link"><p class="CATALOG-2">Make it Happen : Surrender Your Fear. Take the Leap. Live on Purpose.</p></div>
+            <div id="breadcrumb">
+                <div><a href="./catalog.php">CATALOGUE</a> /</div>
+                <div><a href="">ROMANCE</a> /</div>
+                <p>Make it Happen : Surrender Your Fear. Take the Leap. Live on Purpose.</p>
             </div>
             <div class="title">
-            <h1>Détails de l’ouvrage</h1>
-            <span class="material-symbols-outlined">edit</span>
-        </div>
-        <div class="book-details">
-            <div class="cover">
-                <img src="" alt="couverture du livre">
+                <h1>Détails de l’ouvrage</h1>
+                <span class="material-symbols-outlined">edit</span>
             </div>
-            <div class="title-author">
-                <p class="book-title">Make it Happen : Surrender Your Fear. Take the Leap. Live on Purpose.</p>
-                <p class="author">Ajouté par XXXXXXXXX</p>
-            </div>
-            <div class="infos">
-                <div class="review">
-                        <p class="avis">Moyenne des avis : 4</p>
-                        <div class="stars">
-                            <img class="star" src="img/star-12.svg" />
-                            <img class="star" src="img/star-12.svg" />
-                            <img class="star" src="img/star-12.svg" />
-                            <img class="star" src="img/star-12.svg" />
-                            <img class="star" src="img/star-11.svg" />
+            <div id="book-details">
+                <div class="cover">
+                    <img src="" alt="couverture du livre">
+                </div>
+                <div class="infos">
+                    <div class="title-author">
+                        <p class="book-title">Make it Happen : Surrender Your Fear. Take the Leap. Live on Purpose.</p>
+                        <p class="author">Ajouté par</p>
+                        <p class="author">XXXXXXXXX</p>
+                    </div>
+                    <div class="review">
+                            <p>Moyenne des avis : 4</p>
+                            <div class="stars">
+                                <img class="star" src="img/star-12.svg" alt="étoile">
+                                <img class="star" src="img/star-12.svg" alt="étoile">
+                                <img class="star" src="img/star-12.svg" alt="étoile">
+                                <img class="star" src="img/star-12.svg" alt="étoile">
+                                <img class="star" src="img/star-11.svg" alt="étoile">
+                            </div>
+                    </div>
+                    <p id="summary">
+                        Lorem ipsum dolor sit amet consectetur. Sed ornare praesent enim elementum tincidunt viverra ipsum. Amet
+                        orci volutpat in morbi senectus enim est consectetur sodales. Lacus amet orci elementum sed morbi elit
+                        viverra risus. Et urna semper blandit et venenatis nulla adipiscing amet.
+                    </p>
+                    <div id="infos-1">
+                        <div id="catgories-details">
+                                <p>Auteur</p>
+                                <p>Editeur</p>
+                                <p>Année d’édition</p>
+                                <p>Nombre de pages</p>
+                                <p>Catégorie</p>
                         </div>
-                </div>
-                <p class="summary">
-                    Lorem ipsum dolor sit amet consectetur. Sed ornare praesent enim elementum tincidunt viverra ipsum. Amet
-                    orci volutpat in morbi senectus enim est consectetur sodales. Lacus amet orci elementum sed morbi elit
-                    viverra risus. Et urna semper blandit et venenatis nulla adipiscing amet.
-                </p>
-                <div class="catgories-details">
-                        <p>Auteur</p>
-                        <p>Editeur</p>
-                        <p>Année d’édition</p>
-                        <p>Nombre de pages</p>
-                        <p>Catégorie</p>
-                </div>
-                <div class="infos-2">
-                        <p>XXXXXXXXXXX</p>
-                        <p>XXXXXXXXX</p>
-                        <p>2023</p>
-                        <p>245</p>
-                        <p>Fiction</p>
+                        <div class="infos-2">
+                                <p>XXXXXXXXXXX</p>
+                                <p>XXXXXXXXX</p>
+                                <p>2023</p>
+                                <p>245</p>
+                                <p>Fiction</p>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="evaluation">
                 <p>Evaluez cet ouvrage</p>
                 <div class="stars-2">
-                    <img class="étoile" src="" >
-                    <img class="étoile" src="" >
-                    <img class="étoile" src="" >
-                    <img class="étoile" src="" >
-                    <img class="étoile" src="" >
+                    <img class="star" src="" alt="étoile">
+                    <img class="star" src="" alt="étoile">
+                    <img class="star" src="" alt="étoile">
+                    <img class="star" src="" alt="étoile">
+                    <img class="star" src="" alt="étoile">
                 </div>
             </div>
-        </div>
         </main>
         <footer class="footer">
             <? include("./src/parts/footer.inc.php"); ?>
