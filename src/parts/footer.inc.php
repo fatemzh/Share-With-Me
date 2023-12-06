d19543e9bf0ea66bcd64b67aab188ded1e498312
cyberkathleen <67641959+cyberkathleen@users.noreply.github.com>
2023-12-06 11:10:09 +0100

    Footer <hr> not ok

--- src/parts/footer.inc.php
+++ src/parts/footer.inc.php
@@ -1,3 +1,18 @@
 <footer>
-    <h2>Share With Me</h2>
+    <div id="footer-container">
+        <h2>Share With Me</h2>
+        <a href="mailto:info@sharewithme.ch">info@sharewithme.ch</a>
+        <nav>
+            <ul>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="catalog.php">Catalogue</a></li>
+                <li><a href="addBook.php">Ajouter</a></li>
+            </ul>
+        </nav>
+        <hr/>
+        <div id="footer-end">
+            <p>© 2023 Share With Me. All rights reserved</p>
+            <a href="#top">Back to top</a>
+        </div>
+    </div>
 </footer>
