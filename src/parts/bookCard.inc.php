25e684bcbcd50085724f10d239cc9a8307987867
cyberkathleen <67641959+cyberkathleen@users.noreply.github.com>
2023-12-12 08:51:59 +0100

    Include card

---
+++ src/parts/bookCard.inc.php
@@ -0,0 +1,8 @@
+<div class="book-card">
+    <img src="./img/covers/twilight.jpg" alt="image de couverture du livre">
+    <div class="book-infos">
+        <p class="author">Stephenie Meyer</p>
+        <h3>Twilight</h3>
+        <p>Posté par : Pseudo</p>
+    </div>
+</div>
